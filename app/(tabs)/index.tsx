9924c89c61278a4973731d13180b28555b1a7d5f
Eric Tibratt <eritib08@edu.gislaved.se>
2026-09-01 17:13:33 +0200

    Cleaned up index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,4 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet, Text } from 'react-native';
-
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
+import { StyleSheet, Text } from 'react-native';
 
 export default function HomeScreen() {
   return (
